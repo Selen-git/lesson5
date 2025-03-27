73c819ef1293e3231382d73edb4471d84e526a67
Selen-git <Selen_6966@inbox.ru>
2025-03-28 01:26:32 +0500

    Update lesson05_task2.py

--- 05_lesson/lesson05_task2.py
+++ 05_lesson/lesson05_task2.py
@@ -1,1 +1,11 @@
+from selenium import webdriver
 
+driver = webdriver.Chrome()
+
+driver.get("http://uitestingplayground.com/dynamicid")
+
+button = driver.find_element_by_xpath("//button[contains(@onclick, 'buttonClick')]")
+
+bitton.click()
+
+driver.quit()
