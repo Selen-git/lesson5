48116c141477b4db0202b8ff619456b37cbe1606
Selen-git <Selen_6966@inbox.ru>
2025-03-28 01:09:21 +0500

    Update lesson05_task1.py

--- 05_lesson/lesson05_task1.py
+++ 05_lesson/lesson05_task1.py
@@ -1,1 +1,10 @@
+from selenium import webdriver
 
+driver = webdriver.Chrome()
+
+driver.get("http://uitestingplayground.com/classattr
+
+button = driver.find_element_by_css_selector("button.btn-primary")
+button.click()
+
+driver.quit()
