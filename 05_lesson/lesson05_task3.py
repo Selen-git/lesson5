9bbf7c3111362d7e28f5098efbf380565a115fef
Selen-git <Selen_6966@inbox.ru>
2025-03-28 01:50:13 +0500

    Update lesson05_task3.py

--- 05_lesson/lesson05_task3.py
+++ 05_lesson/lesson05_task3.py
@@ -1,1 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+input_field = driver.find_element_by_tag_name("input")
+input_field.send_keys("Sky")
+
+input_field.clear()
+
+input_field.send_keys("Pro")
+
+driver.quit()
