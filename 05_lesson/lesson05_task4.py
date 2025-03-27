ca24967b916d649dea4574b4aa7e5be27cf1dbc6
Selen-git <Selen_6966@inbox.ru>
2025-03-28 02:44:48 +0500

    Update lesson05_task4.py

--- 05_lesson/lesson05_task4.py
+++ 05_lesson/lesson05_task4.py
@@ -1,1 +1,41 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+browser.get('http://the-internet.herokuapp.com/login')
+
+username = browser.find_element_by_id('username')
+username.send_keys('tomsmith')
+
+password = browser.find_element_by_id('password')
+password.send_keys('SuperSecretPassword!')
+
+login_button = browser.find_element_by_xpath('//button[@type="submit"]')
+login_button.click()
+
+succes_message = browser.find_element_by_css_selector('.flash.succes').text
+print(succes_message)
+
+browser.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
