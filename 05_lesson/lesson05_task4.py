6a2b3bc1cee29ff5ddeabc601df28dfd302e4d32
Selen-git <Selen_6966@inbox.ru>
2025-03-28 09:59:51 +0500

    Update lesson05_task4.py

--- 05_lesson/lesson05_task4.py
+++ 05_lesson/lesson05_task4.py
@@ -10,10 +10,10 @@ username.send_keys('tomsmith')
 password = browser.find_element_by_id('password')
 password.send_keys('SuperSecretPassword!')
 
-login_button = browser.find_element_by_xpath('//button[@type="submit"]')
+login_button = browser.find_element('//button[@type="submit"]')
 login_button.click()
 
-succes_message = browser.find_element_by_css_selector('.flash.succes').text
+succes_message = browser.find_element('.flash.succes').text
 print(succes_message)
 
 browser.quit()
